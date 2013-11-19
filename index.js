9bd28531d349477d46567f7ff627bd1ad05d2aac
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-19 09:25:54 -0500

    fixed overwrite

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var exec = require('child_process').exec,
 module.exports = function(inPath, outPath, done){
   var ogrCommand = 'ogr2ogr -f KML '+path.resolve(process.cwd()+'/'+outPath)+ ' '+
                     path.resolve(process.cwd()+'/'+inPath)
-
   exec(ogrCommand, function(err, stdout, stderr){
     done(err)
   })
