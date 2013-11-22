9b1185654d84d0ffaac7348084683c2b7e424c79
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-22 10:05:39 -0500

    switched to absolute paths

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var exec = require('child_process').exec,
     path = require('path')
 
 module.exports = function(inPath, outPath, done){
-  var ogrCommand = 'ogr2ogr -f KML '+path.resolve(process.cwd()+'/'+outPath)+ ' '+
-                    path.resolve(process.cwd()+'/'+inPath)
+  var ogrCommand = 'ogr2ogr -f KML '+outPath+ ' '+inPath
   exec(ogrCommand, function(err, stdout, stderr){
     done(err)
   })
--- test/geojson2kml.js
+++ test/geojson2kml.js
@@ -1,9 +1,10 @@
 var geojson2kml = require('../index.js'),
-    should = require('should')
+    should = require('should'),
+    path = require('path')
 
 describe('geojson2kml', function(){
   it('should take a geojson file and output a kml file', function(done){
-    geojson2kml('./in.geojson', './out.kml', function(err){
+    geojson2kml(path.resolve(__dirname+'/in.geojson'), path.resolve(__dirname+'/out.kml'), function(err){
       if(err) throw err
       done()
     })
